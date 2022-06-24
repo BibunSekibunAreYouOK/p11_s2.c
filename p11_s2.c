682bf10850ae7c9d0057c1b66460b7421fc76f04
BiibunSekibun_jp <ia7903955@gmail.com>
2022-06-24 21:14:17 +0900

    プロジェクト ファイルを追加します。

---
+++ p11_s2.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define N 21
+int main(void)
+{
+	double x[N], y[N], z[N];
+	int i;
+	FILE* ifp, * ofp;
+	ifp = fopen("data1.txt", "r");
+	ofp = fopen("data2.csv", "w");
+	fprintf(ofp, "z=x^3-100*x+50を計算するプログラムを作成し、csvファイル上に書き込む。\n");
+	for (i = 0; i < N; i++)
+	{
+		fscanf(ifp, "%lf,%lf\n", &x[i], &y[i]);
+		printf("x=%f,y=%f\n", x[i], y[i]);
+		z[i] = (x[i] * x[i] * x[i]) - 100 * x[i] + 50;
+		//printf(ofp, "x=%f, y=%f, z=%f¥n", x[i], x[i], z[i]);
+		fprintf(ofp, "x=%f, y=%f, z=%f\n", x[i], y[i], z[i]);
+
+
+	}
+	fclose(ifp); fclose(ofp);
+	return 0;
+}
